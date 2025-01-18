8d05c881b91706d801c899f3e88131fd117ab05a
Pradeep Nayak <pradpnayak@gmail.com>
2025-01-18 12:00:28 +0000

    civix upgrade

--- formatdateexportcolumn.php
+++ formatdateexportcolumn.php
@@ -13,15 +13,6 @@ function formatdateexportcolumn_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function formatdateexportcolumn_civicrm_xmlMenu(&$files) {
-  _formatdateexportcolumn_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,96 +22,12 @@ function formatdateexportcolumn_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function formatdateexportcolumn_civicrm_postInstall() {
-  _formatdateexportcolumn_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function formatdateexportcolumn_civicrm_uninstall() {
-  _formatdateexportcolumn_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function formatdateexportcolumn_civicrm_enable() {
   _formatdateexportcolumn_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function formatdateexportcolumn_civicrm_disable() {
-  _formatdateexportcolumn_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function formatdateexportcolumn_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _formatdateexportcolumn_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function formatdateexportcolumn_civicrm_managed(&$entities) {
-  _formatdateexportcolumn_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function formatdateexportcolumn_civicrm_caseTypes(&$caseTypes) {
-  _formatdateexportcolumn_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function formatdateexportcolumn_civicrm_angularModules(&$angularModules) {
-  _formatdateexportcolumn_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function formatdateexportcolumn_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _formatdateexportcolumn_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
